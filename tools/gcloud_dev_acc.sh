4cdfb4c49f217ab3ca335898e565ee6b0d98aa78
Jeremy Andrews <jandrews@venturseed.com>
2024-01-26 13:11:23 -0500

    devops tools set

---
+++ tools/gcloud_dev_acc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#cat "$vs_dev_service_creds" >"$(pwd)/service-account.json"
+
+gcloud config set project dartstream-dev
+
+gcloud auth activate-service-account $ACCOUNT_DEV --key-file $GOOGLE_CLOUD_CREDENTIALS_DEV
+
+
+# npm For Vite production assets
+which npm
+echo $PATH
+apk add nodejs npm
+cd dartstream || exit
+npm i vite 
+npm run prod
