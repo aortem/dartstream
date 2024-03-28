fb5bb6f35f37063967b749dccea2078e75d4e219
Usman <ubabar@venturseed.com>
2024-03-28 18:01:58 +0500

    fix issue

--- dartstream/lib/backend/src/routing/dynamic_route.dart
+++ dartstream/lib/backend/src/routing/dynamic_route.dart
@@ -7,13 +7,10 @@ Map<String, String> parsePath(String path) {
 
   // Start at index 2 to skip the first part (empty string) and the resource name
   for (var i = 1; i < parts.length; i += 2) {
-    print(parts.length);
     if (i + 1 < parts.length) {
-      print("aaaa ${[parts[i]]}");
       params[parts[i]] = parts[i + 1];
     }
   }
-  print("parsepath params ${params}");
   return params;
 }
 
@@ -21,16 +18,13 @@ Map<String, String> parsePath(String path) {
 void handleRequest(HttpRequest request) {
   final path = request.uri.path;
   final params = parsePath(path);
-  print(path);
-  print(params);
 
   // Define logic for different routes based on path and parameters
   if (path == '/users') {
     // Handle request for all users (replace with actual logic)
     request.response.write('Fetching all users...');
   } else if (path.startsWith('/users/') && path.length > 7) {
-    final userId = params['id'];
-    print("sasasa${userId}");
+    final userId = params['users'];
     if (userId != null) {
       // Handle request for specific user by ID (replace with actual logic)
       request.response.write('Fetching user with ID $userId...');
