2d502a8a96b6aee7111b42c41d1ea27cf2f41178
Jeremy Andrews <jandrews@venturseed.com>
2024-03-29 09:31:12 -0400

    changed 'tools' to 'tooling' to avoid reserved namespace issue

---
+++ dartstream/lib/backend/packages/tooling/overrides/ds_custom_package.dart
@@ -0,0 +1,9 @@
+/* import 'package:some_package/some_package.dart';
+//Only USE IF NECESSARY
+class CustomSomeClass extends SomeClassFromPackage {
+  @override
+  void someMethod() {
+    // Custom implementation
+  }
+}
+*/
