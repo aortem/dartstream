89d74fec930e5aa5c5994b2842a7dbd2f17f2876
Jeremy Andrews <jandrews@venturseed.com>
2024-04-01 16:42:33 -0400

    update local dependency management

--- dartstream/lib/backend/packages/tooling/cli_util/lib/core/ds_create_shelf.dart
+++ dartstream/lib/backend/packages/tooling/cli_util/lib/core/ds_create_shelf.dart
@@ -1,6 +1,6 @@
 // Import necessary packages
 import 'dart:io';
-//import 'package:ds_dartstream/';
+import 'package:ds_shelf/core/ds_shelf_project_template.dart';
 import 'package:args/command_runner.dart';
 //import 'your_project_structure.dart'; // Import your project scaffolding utilities  To DO
 
