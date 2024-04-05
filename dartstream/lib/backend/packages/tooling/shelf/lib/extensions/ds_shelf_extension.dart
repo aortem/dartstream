e0cd77e80596b5ed2fddfdfbcb7815a66d7886e9
Jeremy Andrews <jandrews@venturseed.com>
2024-04-05 10:03:47 -0400

    update shelf

--- dartstream/lib/backend/packages/tooling/shelf/lib/extensions/ds_shelf_extension.dart
+++ dartstream/lib/backend/packages/tooling/shelf/lib/extensions/ds_shelf_extension.dart
@@ -4,6 +4,8 @@ import 'package:ds_shelf/ds_shelf.dart'; //Coverage for other packages
 
 //Import other core packages
 
+abstract class DSStaticFileHanlder extends DSShelfCore {}
+
 extension StaticFileHandler on Router {
   void serveStaticFiles(String path) {
     // Implementation for serving static files
