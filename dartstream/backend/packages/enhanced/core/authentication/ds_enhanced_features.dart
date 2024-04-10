c682d6909149251dbeab23796f9f0941f6b2b88c
Jeremy Andrews <jandrews@venturseed.com>
2024-04-10 08:46:43 -0400

    remove top end lib folder for publication preparation

---
+++ dartstream/backend/packages/enhanced/core/authentication/ds_enhanced_features.dart
@@ -0,0 +1,43 @@
+// lib/ds_shelf.dart
+library ds_enhanced_features;
+
+//The Ehanced features include SDKs, trusted third party integrations.
+
+//AUTHENTICATION
+import 'package:amazon_cognito_identity_dart_2/cognito.dart';
+import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
+
+// From anywhere in the lib directory
+
+//DATABASE
+
+//FILE STORAGE
+
+//PAYMENTS
+
+//Export our Enhanced Feture Libraries
+
+// AUTHENTICATION PACKAGES
+
+export 'lib/core/authentication/amazon_cognito_sdk.dart'; // Amazon SDK Package
+export 'lib/core/authentication/azure_identity_sdk.dart'; // Azure SDK Package
+export 'lib/core/authentication/azure_identity_sdk.dart'; // Firebase Admin SDK Package
+
+// DATABASE PACKAGES
+export '../core/ds_shelf_core_export.dart'; // Exporting your core classes
+export '../api/ds_shelf_api_export.dart'; // Exporting your api classes
+export '../extensions/ds_shelf_extensions_export.dart'; // Exporting your extensions classes
+export '../utilities/ds_shelf_utilities_export.dart'; // Exporting your utility classes
+export '../overrides/ds_shelf_overrides_export.dart'; // Exporting your overrides classes
+
+//FILESTORE PACKAGES
+
+//PAYMENTS PACKAGES
+
+//Test part - to be removed
+
+//part 'api/ds_shelf_api.dart';
+//part 'core/ds_shelf_core.dart';
+//part 'extensions/ds_shelf_extension.dart';
+//part 'overrides/ds_overrides.dart';
+//part 'utilities/ds_utilities_base.dart'; */
