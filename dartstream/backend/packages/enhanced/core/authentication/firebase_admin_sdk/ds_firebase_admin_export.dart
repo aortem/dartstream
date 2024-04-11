6403f291d60b594033aa441971927542118349d9
Jeremy Andrews <jandrews@venturseed.com>
2024-04-11 09:46:37 -0400

    import standard package dependencies

--- dartstream/backend/packages/enhanced/core/authentication/firebase_admin_sdk/ds_firebase_admin_export.dart
+++ dartstream/backend/packages/enhanced/core/authentication/firebase_admin_sdk/ds_firebase_admin_export.dart
@@ -1,7 +1,7 @@
 library ds_amazon_cognito_core;
 
 // Export main core module
-export 'ds_amazon_cognito_core.dart';
+export 'ds_firebase_admin_core.dart';
 
 
 // You can also include any shared utility functions or common interfaces here that might be used across multiple utility files.
--- dartstream/backend/packages/standard/ds_standard_features.dart
+++ dartstream/backend/packages/standard/ds_standard_features.dart
@@ -1,7 +1,7 @@
 // lib/ds_shelf.dart
 library ds_standard_features;
 
-//The Shelf Core Libraries - Built by the dart team
+//The Standard Core Libraries - Built by the dart team
 
 
 
@@ -13,10 +13,3 @@ export 'extensions/ds_shelf_extensions_export.dart'; // Exporting your extension
 export 'utilities/ds_shelf_utilities_export.dart'; // Exporting your utility classes
 export 'overrides/ds_shelf_overrides_export.dart'; // Exporting your overrides classes
 
-//Test part - to be removed
-
-//part 'api/ds_shelf_api.dart';
-//part 'core/ds_shelf_core.dart';
-//part 'extensions/ds_shelf_extension.dart';
-//part 'overrides/ds_overrides.dart';
-//part 'utilities/ds_utilities_base.dart'; */
