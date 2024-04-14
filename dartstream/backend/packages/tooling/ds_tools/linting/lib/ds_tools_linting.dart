ceb84a44da4032453618c2986e22f7c292af7db2
Jeremy Andrews <jandrews@venturseed.com>
2024-04-14 17:01:34 -0400

    adding lib folder for standard packages to get picked up

---
+++ dartstream/backend/packages/tooling/ds_tools/linting/lib/ds_tools_linting.dart
@@ -0,0 +1,23 @@
+// lib/ds_tools_linting.dart
+library ds_tools_linting;
+
+//Import DS Tools Linting Libraries
+
+import 'package:pede;
+import 'package:build_test/build_test.dart';
+import 'package:coverage/coverage.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+
+// Export DS Tools Linting Libraries
+export 'package:build_runner/build_runner.dart';
+export 'package:build_test/build_test.dart';
+export 'package:coverage/coverage.dart';
+export 'package:mocktail/mocktail.dart'; // On By Default
+export 'package:test/test.dart';
+
+//Export our Core Libraries
+export 'extensions/ds_tools_linting_extensions_export.dart'; // Exporting your extensions classes
+
+
---
+++ dartstream/backend/packages/tooling/ds_tools/linting/lib/extensions/ds_tools_linting_extensions_export.dart
@@ -0,0 +1,8 @@
+library ds_tools_testing_extension;
+
+// Export main core module
+export 'ds_tools_linting_extension.dart';
+
+
+// You can also include any shared utility functions or common interfaces here that might be used across multiple utility files.
+
