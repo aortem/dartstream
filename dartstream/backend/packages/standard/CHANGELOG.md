e442fcdf8ea340407cccfea790d7f3ed36a30421
Jeremy Andrews <jandrews@venturseed.com>
2024-04-13 21:01:19 -0400

    update latest packages and tooling

---
+++ dartstream/backend/packages/standard/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [0.0.1-pre] - 2024-04-10
+
+### Added
+- Initial release with standard features framework functionality.
+
+
+
---
+++ dartstream/backend/packages/standard/api/ds_standard_api.dart
@@ -0,0 +1,6 @@
+// Import Top Level Package
+import 'package:ds_standard_features/ds_standards_features.dart'; //Standard Dartstream Features
+
+//Import other core packages
+
+class DSStandardApiCore {}
