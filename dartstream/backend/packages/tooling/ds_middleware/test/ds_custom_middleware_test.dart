d2e8cf05f5778f57ed7a568e5da46dbe88a14ad7
Jeremy Andrews <jandrews@venturseed.com>
2024-04-22 10:40:01 -0400

    update middleware

--- dartstream/backend/packages/tooling/ds_middleware/test/ds_custom_middleware_test.dart
+++ dartstream/backend/packages/tooling/ds_middleware/test/ds_custom_middleware_test.dart
@@ -264,18 +264,17 @@
 //   });
 // }
 import 'package:ds_custom_middleware/ds_custom_middleware.dart';
-import 'package:http/http.dart';
-import 'package:test/test.dart';
+import 'package:ds_standard_features/ds_standard_features.dart'; //as dsmahca;
+import 'package:ds_tools_testing/ds_tools_testing.dart';
 
 void main() {
   test('Middleware chain runs correctly', () async {
-    
     final middlewareChain = <Middleware>[
       (Request request, Handler handler) async {
         // First middleware logic
         print('First middleware');
-         print('request $request');
-          print('handler $handler');
+        print('request $request');
+        print('handler $handler');
         final response = await handler(request);
         // Additional logic if needed
         return response;
@@ -288,15 +287,16 @@ void main() {
         return response;
       },
     ];
- final response1 = await myRequestHandler(Request('GET', Uri.parse("https://example.com")));
+    final response1 = await myRequestHandler(
+        Request('GET', Uri.parse("https://example.com")));
     print(response1.body); // Print the response body
-    expect(response1.statusCode, equals(200)); 
+    expect(response1.statusCode, equals(200));
     final response = await runMiddlewareChain(
         Request('GET', Uri.parse("https://example.com")),
         myRequestHandler,
         middlewareChain);
-        
 
-    expect(response.statusCode, equals(200)); // Assuming myRequestHandler returns 200
+    expect(response.statusCode,
+        equals(200)); // Assuming myRequestHandler returns 200
   });
 }
