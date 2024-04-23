b3b3d1db1ffaf805b0c0184549a23ac82f27c61c
Usman <ubabar@venturseed.com>
2024-04-23 19:02:47 +0500

    update test file

--- dartstream/backend/packages/tooling/ds_middleware/test/ds_custom_middleware_test.dart
+++ dartstream/backend/packages/tooling/ds_middleware/test/ds_custom_middleware_test.dart
@@ -1,11 +1,12 @@
 import 'package:ds_custom_middleware/ds_custom_middleware.dart';
-import 'package:ds_standard_features/ds_standard_features.dart'; //as dsmahca;
-import 'package:ds_tools_testing/ds_tools_testing.dart';
+import 'package:ds_standard_features/ds_standard_features.dart' as ds_standard_features;
+import 'package:ds_tools_testing/ds_tools_testing.dart' as ds_match; //as dsmahca;
+// import 'package:ds_tools_testing/ds_tools_testing.dart';
 
 void main() {
-  test('Middleware chain runs correctly', () async {
+  ds_match.test('Middleware chain runs correctly', () async {
     final middlewareChain = <Middleware>[
-      (Request request, Handler handler) async {
+      (ds_standard_features.Request request, Handler handler) async {
         // First middleware logic
         print('First middleware');
         print('request $request');
@@ -14,7 +15,7 @@ void main() {
         // Additional logic if needed
         return response;
       },
-      (Request request, Handler handler) async {
+      (ds_standard_features.Request request, Handler handler) async {
         // Second middleware logic
         print('Second middleware');
         final response = await handler(request);
@@ -23,15 +24,15 @@ void main() {
       },
     ];
     final response1 = await myRequestHandler(
-        Request('GET', Uri.parse("https://example.com")));
+        ds_standard_features.Request('GET', Uri.parse("https://example.com")));
     print(response1.body); // Print the response body
-    expect(response1.statusCode, equals(200));
+    ds_match.expect(response1.statusCode,  ds_match.equals(200));
     final response = await runMiddlewareChain(
-        Request('GET', Uri.parse("https://example.com")),
+       ds_standard_features.Request('GET', Uri.parse("https://example.com")),
         myRequestHandler,
         middlewareChain);
 
-    expect(response.statusCode,
-        equals(200)); // Assuming myRequestHandler returns 200
+    ds_match.expect(response.statusCode,
+        ds_match.equals(200)); // Assuming myRequestHandler returns 200
   });
 }
