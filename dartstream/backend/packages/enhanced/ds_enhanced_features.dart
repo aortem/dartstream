87784fb244df1a0328cdc562cf77555aed0e22c0
Jeremy Andrews <jandrews@venturseed.com>
2024-04-26 10:13:23 -0400

    update the readme

--- dartstream/backend/packages/enhanced/ds_enhanced_features.dart
+++ dartstream/backend/packages/enhanced/ds_enhanced_features.dart
@@ -1,26 +1,55 @@
 // lib/ds_enchanced_features.dart
 library ds_enhanced_features;
 
-//IMPORTS
+//------------------ IMPORTS ------------------
 
-//BASE MIDDLEWARE LIBRIARIES - USE ONLY ONE
-
-//The Base Dartstream and Custom Shelf Library.
+//DS Shelf and Custom Middleware - Use Only One
 
 import 'package:ds_shelf/ds_shelf.dart';
+import 'package:ds_standard_features/ds_standard_features.dart';
 
 //import 'package:ds_custom/ds_custom.dart';
 
-//Import Enhanced libraries.
+//------------------ AUTHENICATION PACKAGES ------------------
 
+//Amazon SDK
+//Firebase ADMIN SDK (GCP)
+//Azure SDK
+
+//------------------ DATABASE PACKAGES ------------------
 import 'package:sqlite3/sqlite3.dart'; // Local development only
 
-//EXPORTS
+// Amazon Database
+// GCP Database
+// Azure Database
+
+//------------------ FILE_STORAGE PACKAGES ------------------
+
+// Amazon File Storage
+// GCP File Storage
+// Azure File Storage
+
+//------------------ GENERAL_THIRD_PARTY------------------
+
+//------------------ GOOGLE_DEV PACKAGES ------------------
+
+/*
+
+Welcome to use any of the packages here https://pub.dev/publishers/google.dev/packages 
+
+*/
+
+//------------------ PAYMENTS ------------------
+
+//Stripe
+//Paypal
+
+//------------------ EXPORTS ------------------
 
 export 'package:ds_shelf/ds_shelf.dart';
 //export 'package:ds_custom/ds_custom.dart';
 
-//Export our Enchanced Libraries
+//THIRD PARTY AUTHENTICATION PACKAGES
 
 export 'package:sqlite3/sqlite3.dart'; // Local development only
 
