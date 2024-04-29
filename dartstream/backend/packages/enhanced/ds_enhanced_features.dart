122be315b4a2e66ed671d934e1196c75bcab9379
Jeremy Andrews <jandrews@venturseed.com>
2024-04-29 07:50:00 -0400

    update enhanced package structure

--- dartstream/backend/packages/enhanced/ds_enhanced_features.dart
+++ dartstream/backend/packages/enhanced/ds_enhanced_features.dart
@@ -1,10 +1,11 @@
 // lib/ds_enchanced_features.dart
 library ds_enhanced_features;
 
-//------------------ IMPORTS ------------------
+//------------------ IMPORTS ---------------------------------
 
-//Import Standard Libraries and Tools
+//------------------ STANDARD, MIDDLEWARE, TOOLS PACKAGES ------------------
 
+//Import Standard Libraries
 import 'package:ds_standard_features/ds_standard_features.dart';
 
 //DS Shelf and Custom Middleware - Use Only One
@@ -48,14 +49,46 @@ Welcome to use any of the packages here https://pub.dev/publishers/google.dev/pa
 
 //------------------ EXPORTS ------------------
 
+//------------------ STANDARD, MIDDLEWARE, TOOLS PACKAGES ------------------
+
+//Export Standard Libraries
+export 'package:ds_standard_features/ds_standard_features.dart';
+
+//DS Shelf and Custom Middleware - Use Only One
 export 'package:ds_shelf/ds_shelf.dart';
+
 //export 'package:ds_custom/ds_custom.dart';
 
-//THIRD PARTY AUTHENTICATION PACKAGES
-
-export 'package:sqlite3/sqlite3.dart'; // Local development only
+//-------------------AUTHENTICATION PACKAGES -----------------
 
 export 'core/authentication/amazon_cognito_sdk/ds_amazon_cognito_export.dart';
-//export 'api/file.dart';
-//export 'extensions/file.dart';
-//export 'overrides/file.dart';
+//export 'core/authentication/azure_identity_sdk/ds_azure_identity_export.dart';
+//export 'core/authentication/firebase_admin_sdk/ds_firebase_admin__export.dart';
+
+//------------------ DATABASE PACKAGES ------------------
+export 'package:sqlite3/sqlite3.dart'; // Local development only
+
+// Amazon Database
+// GCP Database
+// Azure Database
+
+//------------------ FILE_STORAGE PACKAGES ------------------
+
+// Amazon File Storage
+// GCP File Storage
+// Azure File Storage
+
+//------------------ GENERAL_THIRD_PARTY------------------
+
+//------------------ GOOGLE_DEV PACKAGES ------------------
+
+/*
+
+Welcome to use any of the packages here https://pub.dev/publishers/google.dev/packages 
+
+*/
+
+//------------------ PAYMENTS ------------------
+
+//Stripe
+//Paypal
