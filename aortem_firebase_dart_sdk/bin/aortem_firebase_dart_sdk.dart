7af3f757cba60a7077b4d54599cad0c3582ed4d0
Satendra Pal <spal@aortem.io>
2024-05-31 01:01:22 +0530

    skeleton

---
+++ aortem_firebase_dart_sdk/bin/aortem_firebase_dart_sdk.dart
@@ -0,0 +1,5 @@
+import 'package:aortem_firebase_dart_sdk/aortem_firebase_dart_sdk.dart' as aortem_firebase_dart_sdk;
+
+void main(List<String> arguments) {
+  print('Hello Firebase Sdk: !');
+}
---
+++ aortem_firebase_dart_sdk/test/aortem_firebase_dart_sdk_test.dart
@@ -0,0 +1,8 @@
+import 'package:aortem_firebase_dart_sdk/aortem_firebase_dart_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(null, 42);
+  });
+}
