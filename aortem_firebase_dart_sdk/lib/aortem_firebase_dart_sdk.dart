eaadb3cfa4da506c04fd909b65f888e16fb186ab
Usman <ubabar@venturseed.com>
2024-06-19 23:54:33 +0500

    issue# 25, 26, 27 are completed

---
+++ aortem_firebase_dart_sdk/lib/aortem_firebase_dart_sdk.dart
@@ -0,0 +1,8 @@
+library aortem_firebase_dart_sdk;
+
+//Import firebase_dart
+//required to control CLI commands
+import 'package:firebase_dart/firebase_dart.dart';
+// Export CLI Args
+
+export 'src/firebase_auth_service.dart';
