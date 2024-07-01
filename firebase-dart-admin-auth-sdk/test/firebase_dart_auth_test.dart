21635be593792a8e1d6b0bad35683fbe33941c18
Jeremy Andrews <jandrews@venturseed.com>
2024-07-01 17:41:53 -0400

    update correct package naming convention

--- firebase-dart-admin-auth-sdk/test/firebase_dart_auth_test.dart
+++ firebase-dart-admin-auth-sdk/test/firebase_dart_auth_test.dart
@@ -1,6 +1,6 @@
 import 'package:ds_tools_testing/ds_tools_testing.dart';
 import 'package:ds_standard_features/ds_standard_features.dart' as http;
-import 'package:firebase_dart_admin_auth_sdk/firebase_dart_auth.dart';
+import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
 import 'package:firebase_dart_admin_auth_sdk/src/action_code_settings.dart'
     as acs;
 
