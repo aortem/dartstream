6651e0d11be314d249428664b833f82c9563cd94
Jeremy Andrews <jandrews@venturseed.com>
2024-07-20 12:48:28 -0400

    update sample app folder

---
+++ firebase-dart-admin-auth-sample-app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<google_sign_in_ios/FLTGoogleSignInPlugin.h>)
+#import <google_sign_in_ios/FLTGoogleSignInPlugin.h>
+#else
+@import google_sign_in_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
+}
+
+@end
