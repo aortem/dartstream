d6a2c9ff48ca3728a3d97f0da5f9c092f61d0143
Jeremy Andrews <jandrews@venturseed.com>
2024-08-07 14:41:16 +0000

    Revert "Merge branch 'issue-36-to-41' into 'development'"
    This reverts merge request !32

--- firebase-dart-admin-auth-sdk/lib/src/auth/auth_base.dart
+++ firebase-dart-admin-auth-sdk/lib/src/auth/auth_base.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_dart_admin_auth_sdk/src/firebase_auth.dart';
-import 'package:firebase_dart_admin_auth_sdk/src/http_response.dart';
 
 abstract class AuthBase {
   final FirebaseAuth auth;
 
   AuthBase(this.auth);
 
-  Future<HttpResponse> performRequest(
+  Future<Map<String, dynamic>> performRequest(
       String endpoint, Map<String, dynamic> body) {
     return auth.performRequest(endpoint, body);
   }
--- firebase-dart-admin-auth-sdk/lib/src/user.dart
+++ firebase-dart-admin-auth-sdk/lib/src/user.dart
@@ -3,21 +3,16 @@
 //Added getIdToken method for token management
 //added toMap method for easy serialization
 
-import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
-import 'package:firebase_dart_admin_auth_sdk/src/auth_credential.dart';
-
-import 'firebase_auth.dart';
-import 'id_token_result_model.dart';
-
 class User {
   final String uid;
-  final String? email;
-  final bool emailVerified;
-  final String? phoneNumber;
-  final String? displayName;
-  final String? photoURL;
-  String? _idToken;
-  DateTime? _idTokenExpiration;
+  String? email;
+  bool? emailVerified;
+  String? phoneNumber;
+  String? displayName;
+  String? photoURL;
+  String? idToken;
+  String? idTokenExpiration;
+  String? refreshToken;
 
   User({
     required this.uid,
@@ -26,37 +21,21 @@ class User {
     this.phoneNumber,
     this.displayName,
     this.photoURL,
+    this.idToken,
+    this.idTokenExpiration,
+    this.refreshToken,
   });
-  @override
-  String toString() {
-    return 'User{uid: $uid, email: $email, emailVerified: $emailVerified, phoneNumber: $phoneNumber, displayName: $displayName, photoURL: $photoURL, _idToken: $_idToken, _idTokenExpiration: $_idTokenExpiration}';
-  }
 
   bool get isAnonymous => email == null && phoneNumber == null;
 
   Future<String> getIdToken([bool forceRefresh = false]) async {
-    if (forceRefresh ||
-        _idToken == null ||
-        _idTokenExpiration == null ||
-        DateTime.now().isAfter(_idTokenExpiration!)) {
+    if (forceRefresh || idToken == null || idTokenExpiration == null) {
       // In a real implementation, make an API call to refresh the token here
       // For this, just simulate a token refresh
-      _idToken = 'refreshed_token_${DateTime.now().millisecondsSinceEpoch}';
-      _idTokenExpiration = DateTime.now().add(Duration(hours: 1));
+      idToken = 'refreshed_token_${DateTime.now().millisecondsSinceEpoch}';
+      // _idTokenExpiration = DateTime.now().add(Duration(hours: 1));
     }
-    return _idToken!;
-  }
-
-  Future<IdTokenResult> getIdTokenResult([bool forceRefresh = false]) async {
-    final token = await getIdToken(forceRefresh);
-    return IdTokenResult(
-      token: token,
-      expirationTime: _idTokenExpiration?.millisecondsSinceEpoch ?? 0,
-      issuedAtTime: DateTime.now().millisecondsSinceEpoch,
-      signInProvider: 'password', // or 'phone' or '(link unavailable)' etc.
-      userId: uid,
-      authTime: DateTime.now().millisecondsSinceEpoch.toString(),
-    );
+    return idToken!;
   }
 
   Map<String, dynamic> toMap() {
@@ -79,16 +58,23 @@ class User {
       phoneNumber: json['phoneNumber'],
       displayName: json['displayName'],
       photoURL: json['photoUrl'] ?? json['photoURL'],
+      idToken: json['idToken'],
+      idTokenExpiration: json['expiresIn'],
+      refreshToken: json['refreshToken'],
     );
   }
 
-  Future<void> linkWithCredential(AuthCredential credential) async {
-    // final user = await FirebaseApp.instance;
-    //   if (user == null) {
-    //     throw Exception('No user is signed in');
-    //   }
-    //   final updatedUser = await user.linkWithCredential(credential);
-    //   return User.fromJson(updatedUser.toJson());
-    // }
+  User copyWith(User user) {
+    return User(
+      uid: user.uid,
+      displayName: user.displayName ?? displayName,
+      email: user.email ?? email,
+      emailVerified: user.emailVerified ?? emailVerified,
+      idToken: user.idToken ?? idToken,
+      idTokenExpiration: user.idTokenExpiration ?? idTokenExpiration,
+      phoneNumber: user.phoneNumber ?? user.phoneNumber,
+      photoURL: user.photoURL ?? user.photoURL,
+      refreshToken: user.refreshToken ?? refreshToken,
+    );
   }
 }
