7c30a597cf087207c6caf66302a7120ac5afb350
Usman <ubabar@venturseed.com>
2024-08-09 12:02:54 +0500

    add update user form, update use language function, downgrade version ds_standard_features in test folder pubspec.yaml file

--- firebase-dart-admin-auth-sdk/lib/src/auth/user_device_language.dart
+++ firebase-dart-admin-auth-sdk/lib/src/auth/user_device_language.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
@@ -7,17 +8,27 @@ class UseDeviceLanguageService {
 
   UseDeviceLanguageService({required this.auth});
 
-  Future<void> useDeviceLanguage(String userId, String languageCode) async {
+  Future<String?> useDeviceLanguage(String userId, String languageCode) async {
     try {
-      final url = 'update';
+      final url = 'lookup';
       final body = {
         'idToken': userId,
-        'languageCode': languageCode,
       };
 
       final response = await auth.performRequest(url, body);
       if (response.statusCode == 200) {
-        log("Language is set ");
+        final Map<String, dynamic> responseBody = response.body;
+
+        final List users = responseBody['users'] ?? [];
+
+        if (users.isNotEmpty) {
+          final String? languageCode = users[0]['languageCode'];
+          log("No devise Language is set");
+          return languageCode;
+        } else {
+          print('User not found.');
+          return null;
+        }
       } else {
         print('update : ${response.body}');
       }
