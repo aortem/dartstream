88c16de5a634d72397ec13733740a5bb5d2f3a94
Farimah Pirahmadi <fpirahmadi@aortem.io>
2024-08-19 23:14:06 -0700

    unit testing fix to be run on web

---
+++ firebase-dart-admin-auth-sdk/test/app_test.dart
@@ -0,0 +1,6 @@
+import 'package:test/test.dart';
+import 'firebase_dart_auth_test.dart' as firebase_dart_auth_test;
+
+void main() {
+  firebase_dart_auth_test.main();
+}
---
+++ firebase-dart-admin-auth-sdk/test/unsupported.dart
@@ -0,0 +1,4 @@
+// unsupported.dart
+class html {
+  // Define any dummy classes or methods if necessary
+}
