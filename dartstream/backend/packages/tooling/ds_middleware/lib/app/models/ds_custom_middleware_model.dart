bbd5da3a7924dfb6f51b6e3560af03aa48748593
Brian Chebon <bchebon@aortem.io>
2024-08-28 11:33:46 +0300

    create ds_route_params and added its method to ds_custom_middleware_model

--- dartstream/backend/packages/tooling/ds_middleware/lib/app/models/ds_custom_middleware_model.dart
+++ dartstream/backend/packages/tooling/ds_middleware/lib/app/models/ds_custom_middleware_model.dart
@@ -3,12 +3,24 @@ class DsCustomMiddleWareRequest {
   final Uri uri;
   final Map<String, String> headers;
   final body;
+  final Map<String, String> routeParams; // Added routeParams field
 
-  DsCustomMiddleWareRequest(this.method, this.uri, this.headers, this.body);
-  DsCustomMiddleWareRequest change({Map<String, String>? headers}) {
-    // Create a new Request object with the updated headers
+  DsCustomMiddleWareRequest(this.method, this.uri, this.headers, this.body,
+      {this.routeParams = const {}});
+  DsCustomMiddleWareRequest copyWith({
+    String? method,
+    Uri? uri,
+    Map<String, String>? headers,
+    dynamic body,
+    Map<String, String>? routeParams,
+  }) {
     return DsCustomMiddleWareRequest(
-        method, uri, headers ?? this.headers, body);
+      method ?? this.method,
+      uri ?? this.uri,
+      headers ?? this.headers,
+      body ?? this.body,
+      routeParams: routeParams ?? this.routeParams,
+    );
   }
 }
 
