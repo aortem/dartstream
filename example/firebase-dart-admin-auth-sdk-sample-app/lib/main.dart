ad5f8a9626af11464d9bbd27b5e82464c52082c6
Usman <ubabar@venturseed.com>
2024-10-25 00:29:11 +0500

    removed variables

--- example/firebase-dart-admin-auth-sdk-sample-app/lib/main.dart
+++ example/firebase-dart-admin-auth-sdk-sample-app/lib/main.dart
@@ -12,9 +12,9 @@ void main() async {
   if (kIsWeb) {
     //Pass the enviroment variables into the function below, I.E API key and project ID
     FirebaseApp.initializeAppWithEnvironmentVariables(
-        apiKey: 'AIzaSyBli2c-dmD4w2kLHmZU3UtewETvuruVAN4',
-        projectId: 'fire-base-dart-admin-auth-sdk',
-        bucketName: 'fire-base-dart-admin-auth-sdk.appspot.com');
+        apiKey: 'Your Api Key',
+        projectId: 'Your Project Id',
+        bucketName: 'Your Bucket Name');
   } else {
     //  When working with mobile
     if (Platform.isAndroid || Platform.isIOS) {
