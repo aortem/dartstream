64d031e2ce461b3e2dc3cfe653d30f987a2c2c85
Jeremy Andrews <jandrews@venturseed.com>
2024-11-04 13:33:00 -0500

    remove standard web folder per deprecated package

--- dartstream/dartstream_backend/packages/standard_web/lib/api/ds_standard_web_api.dart
+++
@@ -1,6 +0,0 @@
-// Import Top Level Package
-//import 'package:ds';
-
-//Import other core packages
-
-class DSStandardApiCore {}
--- dartstream/dartstream_backend/packages/standard_web/lib/overrides/ds_standard_web_overrides_export.dart
+++
@@ -1,7 +0,0 @@
-library ds_standard_overrides;
-
-// Export standard overrides module
-export 'ds_standard_web_overrides.dart';
-
-
-// You can also include any shared utility functions or common interfaces here that might be used across multiple utility files.
