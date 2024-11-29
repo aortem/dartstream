dc6b0d7613a7ee1109f6871c9d09c89e20f48c89
Jeremy Andrews <jandrews@venturseed.com>
2024-11-29 15:57:47 -0500

    dartstream cleanup and configuration rules

---
+++ tools/pre_dev_deploy2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$SKIP_JOB" == true ]; then
+    echo "Skipping Job"
+    exit
+fi
+
+cd dartstream || exit 
+
+tar -czf dartstream.tgz ./*
+
+gcloud builds submit dartstream.tgz \
+  --tag us-central1-docker.pkg.dev/dartstream-dev/dartstream-dev/dartstream-dev2:"$CI_COMMIT_REF_SLUG"
+
+gcloud run deploy dartstream-dev1 \
+  --image us-central1-docker.pkg.dev/dartstream-dev/dartstream-dev/dartstream-dev2:"$CI_COMMIT_REF_SLUG" \
+  --region us-central1 \
+  --service-account vcf-dev-cloud-cd@dartstream-dev.iam.gserviceaccount.com
