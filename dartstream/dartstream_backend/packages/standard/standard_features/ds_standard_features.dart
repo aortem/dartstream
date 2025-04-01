90d6055b7bf4ea7fcc383b7cb5e02480656b6a01
Jeremy Andrews <jandrews@venturseed.com>
2025-04-01 10:29:18 -0400

    chore: update standard features package for new requirements

--- dartstream/dartstream_backend/packages/standard/standard_features/ds_standard_features.dart
+++ dartstream/dartstream_backend/packages/standard/standard_features/ds_standard_features.dart
@@ -3,10 +3,9 @@ library ds_standard_features;
 
 //------------------ IMPORTS ---------------------------------
 
-import 'ds_standard_features: ^0.0.2;
+import 'package:ds_standard_features/ds_standard_features.dart';
 
 //Export our Standard Libraries
 export '../core/ds_standard_core_export.dart'; // Exporting your core classes
 export '../api/ds_standard_api_export.dart'; // Exporting your api classes
 export '../overrides/ds_standard_overrides_export.dart'; // Exporting your overrides classes
-
