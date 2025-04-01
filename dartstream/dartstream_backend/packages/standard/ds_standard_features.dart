81c8d88c1b167d5bd96c35be36a31040dd5c9848
Jeremy Andrews <jandrews@venturseed.com>
2025-04-01 11:56:53 -0400

    chore:dartstream cleanup

---
+++ dartstream/dartstream_backend/packages/standard/ds_standard_features.dart
@@ -0,0 +1,11 @@
+// lib/ds_standard_features.dart
+library ds_standard_features;
+
+//------------------ IMPORTS ---------------------------------
+
+//Export our Standard Libraries
+/*export '../../core/ds_standard_core_export.dart'; // Exporting your core classes
+export '../../api/ds_standard_api_export.dart'; // Exporting your api classes
+export '../../overrides/ds_standard_overrides_export.dart'; // Exporting your overrides classes */
+
+export 'ds_standard_features.dart';
--- dartstream/dartstream_backend/packages/tooling/lib/ds_tools_testing.dart
+++ dartstream/dartstream_backend/packages/tooling/lib/ds_tools_testing.dart
@@ -1,10 +1,13 @@
 // lib/ds_tools_testing.dart
 library ds_tools_testing;
 
+import 'package:ds_tools_testing/ds_tools_testing.dart';
+
 // Export DS Tools Testing Libraries
 export 'package:build_runner/build_runner.dart';
 export 'package:build_test/build_test.dart';
 export 'package:coverage/coverage.dart';
-export 'package:mocktail/mocktail.dart';
+//export 'package:mocktail/mocktail.dart';
 export 'package:test/test.dart';
+
 //export 'package:mockito/mockito.dart';
