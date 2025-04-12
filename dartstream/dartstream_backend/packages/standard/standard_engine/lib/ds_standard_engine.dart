4c579c4be243be62d3693aac35ad57df9cb1ecc8
Jeremy Andrews <jandrews@venturseed.com>
2025-04-12 15:37:29 -0400

    chore: testing release

--- dartstream/dartstream_backend/packages/standard/standard_engine/lib/ds_standard_engine.dart
+++ dartstream/dartstream_backend/packages/standard/standard_engine/lib/ds_standard_engine.dart
@@ -1,9 +1,2 @@
+// lib/ds_standard_engine.dart
 library ds_standard_engine;
-// This is the main library file for the DartStream Standard Engine package.
-
-//------------------ IMPORTS ---------------------------------
-
-//Export our Standard Libraries
-export './core/ds_standard_core_export.dart'; // Exporting your core classes
-export './api/ds_standard_api_export.dart'; // Exporting your api classes
-export './overrides/ds_standard_overrides_export.dart'; // Exporting your overrides classes */
