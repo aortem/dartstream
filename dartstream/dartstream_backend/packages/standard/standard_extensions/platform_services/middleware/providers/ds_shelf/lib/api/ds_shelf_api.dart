4d501bd5b9ce95ad6b2bee72db80f1d34e0fbb9d
Jeremy Andrews <jandrews@venturseed.com>
2025-06-29 21:50:02 -0400

    chore: update package dependencies

--- dartstream/dartstream_backend/packages/standard/standard_extensions/platform_services/middleware/providers/ds_shelf/lib/api/ds_shelf_api.dart
+++ dartstream/dartstream_backend/packages/standard/standard_extensions/platform_services/middleware/providers/ds_shelf/lib/api/ds_shelf_api.dart
@@ -1,7 +1,9 @@
 // Import Top Level Package
-import 'package:ds_shelf/ds_shelf.dart' as shelf; //Coverage for shelf
-import 'package:ds_shelf/ds_shelf.dart'; //Coverage for other packages
+//import 'package:ds_shelf/ds_shelf.dart' as shelf; //Coverage for shelf
+//import 'package:ds_shelf/ds_shelf.dart'; //Coverage for other packages
 
+import '../ds_shelf.dart';
+import '../ds_shelf.dart' as shelf;
 //Import other core packages
 
 abstract class DSshelf2 extends DSShelfCore {}
