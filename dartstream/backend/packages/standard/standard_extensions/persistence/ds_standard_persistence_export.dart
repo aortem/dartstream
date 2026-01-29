1a13090409d8b45e459484badcd3066a5e6c521d
Jeremy Andrews <jandrews@venturseed.com>
2026-01-29 10:01:02 -0500

    chore: update dependencies

--- dartstream/backend/packages/standard/standard_extensions/persistence/ds_standard_persistence_export.dart
+++ dartstream/backend/packages/standard/standard_extensions/persistence/ds_standard_persistence_export.dart
@@ -2,5 +2,5 @@ library ds_standard_persistence_export;
 
 // Base persistence modules
 export 'database/base/lib/ds_database_base_export.dart';
-export 'logging/base/ds_logging_base_export.dart';
-export 'storage/base/ds_storage_base_export.dart';
+export 'logging/base/lib/ds_logging_base_export.dart';
+export 'storage/base/lib/ds_storage_base_export.dart';
