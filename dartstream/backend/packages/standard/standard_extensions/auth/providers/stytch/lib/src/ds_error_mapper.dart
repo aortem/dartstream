6f818b743a8cdbb5e5e514781e64ac868ddf314e
Svetlana Neogi <sneogi@aortem.io>
2026-02-17 09:13:43 +0530

    chore: clean up stytch provider - remove unused files, switch to ds_tools_testing

--- dartstream/backend/packages/standard/standard_extensions/auth/providers/stytch/lib/src/ds_error_mapper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ds_auth_base/ds_auth_base_export.dart';
-
-class DSStytchErrorMapper {
-  static DSAuthError map(dynamic e) {
-    if (e is DSAuthError) return e;
-    return DSAuthError(e.toString());
-  }
-}
